cd0cda7cbe9df68045bdf4987c1908b88f35b1dc
mahdi3412 <117510962+mahdi3412@users.noreply.github.com>
2026-09-19 13:52:26 +0330

    feat: add auth and creator modules

---
+++ apps/api/src/creators/creators.module.ts
@@ -0,0 +1,5 @@
+import {Module} from "@nestjs/common";
+import {CreatorsController} from "./creators.controller";
+import {CreatorsService} from "./creators.service";
+@Module({controllers:[CreatorsController],providers:[CreatorsService],exports:[CreatorsService]})
+export class CreatorsModule{}
