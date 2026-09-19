f20c6adb927b82d3ee44b8f6692f842ae0b2a4f5
mahdi3412 <117510962+mahdi3412@users.noreply.github.com>
2026-09-19 21:15:50 +0330

    feat(creator-interest): wire non-member interest module

--- apps/api/src/creators/creators.module.ts
+++ apps/api/src/creators/creators.module.ts
@@ -1,6 +1,8 @@
 import {Module} from "@nestjs/common";
 import {CreatorsController} from "./creators.controller";
 import {CreatorsService} from "./creators.service";
+import {CreatorInterestController} from "./creator-interest.controller";
+import {CreatorInterestService} from "./creator-interest.service";
 import {ThresholdsModule} from "../thresholds/thresholds.module";
-@Module({imports:[ThresholdsModule],controllers:[CreatorsController],providers:[CreatorsService],exports:[CreatorsService]})
+@Module({imports:[ThresholdsModule],controllers:[CreatorsController,CreatorInterestController],providers:[CreatorsService,CreatorInterestService],exports:[CreatorsService,CreatorInterestService]})
 export class CreatorsModule{}
