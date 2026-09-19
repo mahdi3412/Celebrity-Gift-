718d7876327baee31e731968dda267a7833bdcbd
mahdi3412 <117510962+mahdi3412@users.noreply.github.com>
2026-09-19 21:16:42 +0330

    fix(database): use centralized pool token in database module

--- apps/api/src/database/database.module.ts
+++ apps/api/src/database/database.module.ts
@@ -2,8 +2,8 @@ import {Global,Module} from "@nestjs/common";
 import {ConfigService} from "@nestjs/config";
 import {Pool} from "pg";
 import {DatabaseBootstrapService} from "./database-bootstrap.service";
+import {PG_POOL} from "./database.constants";
 import {DatabaseService} from "./database.service";
-export const PG_POOL="PG_POOL";
 @Global()
 @Module({providers:[DatabaseBootstrapService,{provide:PG_POOL,useFactory:(config:ConfigService)=>new Pool({connectionString:config.getOrThrow<string>("DATABASE_URL"),max:10}),inject:[ConfigService]},DatabaseService],exports:[PG_POOL,DatabaseService]})
 export class DatabaseModule{}
