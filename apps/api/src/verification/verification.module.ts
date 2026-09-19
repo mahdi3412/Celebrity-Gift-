2752c1eecd1a985944f0a4056f1c6464cce46a0a
mahdi3412 <117510962+mahdi3412@users.noreply.github.com>
2026-09-19 21:13:47 +0330

    feat(kyc): select manual or HyperVerge provider by environment

--- apps/api/src/verification/verification.module.ts
+++ apps/api/src/verification/verification.module.ts
@@ -3,5 +3,7 @@ import {VerificationController} from "./verification.controller";
 import {VerificationService} from "./verification.service";
 import {ManualKycProvider} from "./manual-kyc.provider";
 import {KYC_PROVIDER} from "./kyc.provider";
-@Module({controllers:[VerificationController],providers:[VerificationService,ManualKycProvider,{provide:KYC_PROVIDER,useExisting:ManualKycProvider}],exports:[VerificationService]})
+import {HyperVergeKycProvider} from "./hyperverge.provider";
+import {StorageModule} from "../storage/storage.module";
+@Module({imports:[StorageModule],controllers:[VerificationController],providers:[VerificationService,ManualKycProvider,HyperVergeKycProvider,{provide:KYC_PROVIDER,useFactory:()=>process.env.KYC_PROVIDER==="hyperverge"?new HyperVergeKycProvider():new ManualKycProvider()}],exports:[VerificationService]})
 export class VerificationModule{}
