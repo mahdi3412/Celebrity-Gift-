09faa0dc0ea9b8831b1b5024b13ab626b8f15f64
mahdi3412 <117510962+mahdi3412@users.noreply.github.com>
2026-09-19 13:51:55 +0330

    feat: add web and api foundation

---
+++ apps/api/src/main.ts
@@ -0,0 +1,7 @@
+import "reflect-metadata";
+import {ValidationPipe} from "@nestjs/common";
+import {NestFactory} from "@nestjs/core";
+import helmet from "helmet";
+import {AppModule} from "./app.module";
+async function bootstrap(){const app=await NestFactory.create(AppModule);app.use(helmet());app.enableCors({origin:(process.env.WEB_ORIGIN??"http://localhost:3000").split(","),credentials:true});app.useGlobalPipes(new ValidationPipe({whitelist:true,transform:true,forbidNonWhitelisted:true}));app.setGlobalPrefix("api");await app.listen(Number(process.env.API_PORT??4000));}
+bootstrap();
