cce3360a55c49d472676d289bee28679ac38dd3d
mahdi3412 <117510962+mahdi3412@users.noreply.github.com>
2026-09-19 21:16:28 +0330

    feat(database): wire automatic schema bootstrap service

--- apps/api/src/database/database.module.ts
+++ apps/api/src/database/database.module.ts
@@ -1,8 +1,9 @@
 import {Global,Module} from "@nestjs/common";
 import {ConfigService} from "@nestjs/config";
 import {Pool} from "pg";
+import {DatabaseBootstrapService} from "./database-bootstrap.service";
 import {DatabaseService} from "./database.service";
 export const PG_POOL="PG_POOL";
 @Global()
-@Module({providers:[{provide:PG_POOL,useFactory:(config:ConfigService)=>new Pool({connectionString:config.getOrThrow<string>("DATABASE_URL"),max:10}),inject:[ConfigService]},DatabaseService],exports:[PG_POOL,DatabaseService]})
+@Module({providers:[DatabaseBootstrapService,{provide:PG_POOL,useFactory:(config:ConfigService)=>new Pool({connectionString:config.getOrThrow<string>("DATABASE_URL"),max:10}),inject:[ConfigService]},DatabaseService],exports:[PG_POOL,DatabaseService]})
 export class DatabaseModule{}
