b085284866ed5a4df72b420947ff97eb89eef2a7
mahdi3412 <117510962+mahdi3412@users.noreply.github.com>
2026-09-19 13:53:52 +0330

    feat: add thresholds and dashboards

---
+++ apps/api/src/thresholds/thresholds.module.ts
@@ -0,0 +1,5 @@
+import {Module} from "@nestjs/common";
+import {ThresholdsController} from "./thresholds.controller";
+import {ThresholdsService} from "./thresholds.service";
+@Module({controllers:[ThresholdsController],providers:[ThresholdsService]})
+export class ThresholdsModule{}
