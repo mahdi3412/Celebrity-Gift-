3657eca99dea5e854719d63fbdbbd75d878b97e9
mahdi3412 <117510962+mahdi3412@users.noreply.github.com>
2026-09-19 21:04:18 +0330

    feat(verification): wire KYC provider into verification service

--- apps/api/src/verification/verification.module.ts
+++ apps/api/src/verification/verification.module.ts
@@ -1,5 +1,7 @@
 import {Module} from "@nestjs/common";
 import {VerificationController} from "./verification.controller";
 import {VerificationService} from "./verification.service";
-@Module({controllers:[VerificationController],providers:[VerificationService]})
+import {ManualKycProvider} from "./manual-kyc.provider";
+import {KYC_PROVIDER} from "./kyc.provider";
+@Module({controllers:[VerificationController],providers:[VerificationService,ManualKycProvider,{provide:KYC_PROVIDER,useExisting:ManualKycProvider}],exports:[VerificationService]})
 export class VerificationModule{}
