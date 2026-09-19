3c92ddc4dbba79f13aad9c7788e23d107b5eff63
mahdi3412 <117510962+mahdi3412@users.noreply.github.com>
2026-09-19 13:53:24 +0330

    feat: add verification and station workflow

---
+++ apps/api/src/verification/verification.module.ts
@@ -0,0 +1,5 @@
+import {Module} from "@nestjs/common";
+import {VerificationController} from "./verification.controller";
+import {VerificationService} from "./verification.service";
+@Module({controllers:[VerificationController],providers:[VerificationService]})
+export class VerificationModule{}
